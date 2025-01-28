8cdd79aa3e707ac8af1d38a7dd832cc6d4023ab8
Christopher Debray <christopherdebray1@gmail.com>
2025-01-28 10:55:11 +0100

    feat: init project

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig()
+console.log("🚀 ~ appConfig:", appConfig)
+
+</script>
