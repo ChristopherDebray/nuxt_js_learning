74d71bb2889570fdc8827a24bcd32f4ca8b1dbfc
Christopher Debray <christopherdebray1@gmail.com>
2025-02-04 09:50:56 +0100

    feat: Added task utility componenents (priority and type indicators)

---
+++ components/tasks/utilities/TaskPriorityIcon.vue
@@ -0,0 +1,24 @@
+<template>
+    <Tooltip :text="taskPriority">
+        <UIcon :name="priorityIcon" :class="['w-7 h-7', priorityIconColor[taskPriority]]" />
+    </Tooltip>
+</template>
+
+<script setup lang="ts">
+import { TaskPriority } from '@/enums/tasks/TaskPriority';
+
+const { taskPriority } = defineProps<{
+    taskPriority: TaskPriority
+}>()
+
+const priorityIconColor = {
+    'very high': 'text-red-500',
+    'high': 'text-orange-500',
+    'medium': 'text-yellow-400',
+    'low': 'text-blue-500 rotate-180',
+    'very low': 'text-cyan-400 rotate-180',
+}
+const priorityIcon = [TaskPriority.VERY_LOW, TaskPriority.VERY_HIGH].includes(taskPriority) ? 'i-heroicons-chevron-double-up' : 'i-heroicons-chevron-up';
+</script>
+
+<style scoped></style>
---
+++ components/tasks/utilities/TaskTypeIcon.vue
@@ -0,0 +1,44 @@
+<template>
+  <Tooltip :text="taskType">
+    <span class="flex align-middle bg-gray-400 border-2 border-gray-200 rounded-md">
+      <UIcon :name="taskTypeIcon[taskType].icon" :class="['w-7 h-7', taskTypeIcon[taskType].class]" />
+    </span>
+  </Tooltip>
+</template>
+
+<script lang="ts" setup>
+import { TaskType } from '@/enums/tasks/TaskType';
+
+defineProps<{
+  taskType: TaskType
+}>()
+
+const taskTypeIcon = {
+  [TaskType.EPIC]: {
+    icon: 'material-symbols:crown',
+    class: 'text-violet-400'
+  },
+  [TaskType.BUG]: {
+    icon: 'material-symbols:bug-report',
+    class: 'text-red-600'
+  },
+  [TaskType.FEATURE]: {
+    icon: 'material-symbols:bookmark',
+    class: 'text-orange-400'
+  },
+  [TaskType.TASK]: {
+    icon: 'material-symbols:task',
+    class: 'text-blue-400'
+  },
+  [TaskType.TEST]: {
+    icon: 'material-symbols:biotech-outline',
+    class: 'text-lime-300'
+  },
+  [TaskType.STORY]: {
+    icon: 'material-symbols:book-2',
+    class: 'text-yellow-400'
+  },
+}
+</script>
+
+<style></style>
