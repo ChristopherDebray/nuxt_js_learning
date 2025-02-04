7b8d85b4cb08c507ceaff109a91d3349d2032e97
Christopher Debray <christopherdebray1@gmail.com>
2025-02-04 10:27:07 +0100

    feat: Added DragTaskCard

--- components/tasks/DragTaskCard.vue
+++ components/tasks/DragTaskCard.vue
@@ -5,9 +5,7 @@
     <template #header>
       <div class="justify-between items-center flex">
         <div class="items-center flex">
-          <span class="flex align-middle bg-gray-400 border-2 border-gray-50 rounded-md">
-            <UIcon :name="taskTypeIcon[task.type].icon" :class="['w-7 h-7', taskTypeIcon[task.type].class]" />
-          </span>
+          <TaskTypeIcon :task-type="task.type" />
           <span class="pl-2 ">{{ truncatedTitle }}</span>
         </div>
         <span>{{ task.key }}</span>
@@ -16,20 +14,8 @@
 
     <template #footer>
       <div class="justify-between items-center flex">
-        <UIcon :name="priorityIcon" :class="['w-7 h-7', priorityIconColor[task.priority]]" />
-        <UAvatarGroup size="sm" :max="2">
-          <Tooltip v-if="task.assignee" :text="task.assignee.user_email">
-            <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
-          </Tooltip>
-
-
-          <span class="has-tooltip">
-            <UAvatar v-if="task.assignee" src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" class="has-tooltip" />
-          </span>
-          <span class="has-tooltip">
-            <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
-          </span>
-        </UAvatarGroup>
+        <TaskPriorityIcon :task-priority="task.priority" />
+        <TaskUserGroup :task="task" />
       </div>
     </template>
   </UCard>
@@ -37,60 +23,10 @@
 
 <script lang="ts" setup>
 import type Task from '@/types/tasks/Task';
-import { TaskPriority } from '@/enums/tasks/TaskPriority';
-import { TaskType } from '~/enums/tasks/TaskType';
 
 const props = defineProps<{
   task: Task
 }>()
 
 const truncatedTitle = useTruncate(props.task.title, 20);
-const priorityIconColor = {
-  'very high': 'text-red-500',
-  'high': 'text-orange-500',
-  'medium': 'text-yellow-400',
-  'low': 'text-blue-500 rotate-180',
-  'very low': 'text-cyan-400 rotate-180',
-}
-const priorityIcon = [TaskPriority.VERY_LOW, TaskPriority.VERY_HIGH].includes(props.task.priority) ? 'i-heroicons-chevron-double-up' : 'i-heroicons-chevron-up';
-const taskTypeIcon = {
-  [TaskType.EPIC]: {
-    icon: 'material-symbols:crown',
-    class: 'text-violet-400'
-  },
-  [TaskType.BUG]: {
-    icon: 'material-symbols:bug-report',
-    class: 'text-red-600'
-  },
-  [TaskType.FEATURE]: {
-    icon: 'material-symbols:bookmark',
-    class: 'text-orange-400'
-  },
-  [TaskType.TASK]: {
-    icon: 'material-symbols:task',
-    class: 'text-blue-400'
-  },
-  [TaskType.TEST]: {
-    icon: 'material-symbols:biotech-outline',
-    class: 'text-lime-300'
-  },
-  [TaskType.STORY]: {
-    icon: 'material-symbols:book-2',
-    class: 'text-yellow-400'
-  },
-}
-
-/**
- * task content :
- * 
- * type
- * title
- * key
- * assignee->user_email
- * reporter->user_email
- * priority
- * status->name
- */
 </script>
-
-<style></style>
