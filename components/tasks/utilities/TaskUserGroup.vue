24cbae1b86d0f5ab295b0b90454ffecf721e5c2b
Christopher Debray <christopherdebray1@gmail.com>
2025-02-04 10:27:31 +0100

    feat: Add TaskUserGroup component

---
+++ components/tasks/utilities/TaskUserGroup.vue
@@ -0,0 +1,21 @@
+<template>
+    <UAvatarGroup size="sm" :max="2">
+        <Tooltip v-if="task.assignee" :text="task.assignee.user_email">
+            <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+        </Tooltip>
+
+        <Tooltip :text="task.reporter.user_email">
+            <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+        </Tooltip>
+    </UAvatarGroup>
+</template>
+
+<script setup lang="ts">
+import type Task from '~/types/tasks/Task';
+
+defineProps<{
+    task: Task
+}>()
+</script>
+
+<style scoped></style>
