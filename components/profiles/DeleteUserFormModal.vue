a8f0eef56889b0f2bb599f383be7be6d8ecb7185
Christopher Debray <christopherdebray1@gmail.com>
2025-02-06 13:09:59 +0100

    feat: Add delete user form

---
+++ components/profiles/DeleteUserFormModal.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import type { Form, FormSubmitEvent } from "~/node_modules/@nuxt/ui/dist/runtime/types/form";
+import { DeleteProfileSchema, type DeleteProfileType } from "~/schemas/profiles/profiles";
+import type { FormErrorType } from "~/types/utilities/FormErrorType";
+
+const modal = useModal()
+const emit = defineEmits(['success']);
+const form = ref<Form<DeleteProfileType>>()
+const state = reactive({
+    password: undefined
+})
+
+function onSubmit(event: FormSubmitEvent<DeleteProfileType>) {
+    /** @todo add real api call */
+    try {
+        emit('success')
+        modal.close()
+    } catch (error: any) {
+        if (error.response?.data?.errors) {
+            /** @todo add this as utils function ? */
+            form.value!.setErrors(error.response.data.errors.map((err: FormErrorType) => ({
+                message: err.message,
+                path: err.path,
+            })))
+        }
+    }
+}
+</script>
+
+<template>
+    <UModal>
+        <UCard>
+            <UForm :schema="DeleteProfileSchema" :state="state" class="space-y-4" @submit="onSubmit">
+                <h2 class="text-lg font-medium text--primary">
+                    Are you sure you want to delete your account?
+                </h2>
+
+                <p class="mt-1 text-sm text--secondary">
+                    Once your account is deleted, all of its resources and data will be permanently deleted.
+                    Please enter your password to confirm you would like to permanently delete your account.
+                </p>
+
+                <UFormGroup label="Password" name="password">
+                    <UInput v-model="state.password" type="password" placeholder="Enter your password" />
+                </UFormGroup>
+
+                <UButton @click="modal.close()">Cancel</UButton>
+                <UButton type="submit">Delete account</UButton>
+            </UForm>
+        </UCard>
+    </UModal>
+</template>
---
+++ components/profiles/DeleteUserSection.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import DeleteUserFormModal from '~/components/profiles/DeleteUserFormModal.vue';
+
+const modal = useModal();
+const { notifyError } = useNotification();
+function openDeleteModal() {
+    modal.open(DeleteUserFormModal, {
+        onSuccess() {
+            /** @todo delete account and redirect to homepage */
+            notifyError('The user couldn\'t be deleted.')
+            modal.close()
+        }
+    })
+}
+</script>
+
+<template>
+    <section class="space-y-6">
+        <header>
+            <h2 class="text-lg font-medium text--primary">
+                Delete Account
+            </h2>
+
+            <p class="mt-1 text-sm text--secondary">
+                Once your account is deleted, all of its resources and data will be permanently deleted. Before
+                deleting your account, please download any data or information that you wish to retain.
+            </p>
+        </header>
+
+        <UButton @click="openDeleteModal">Delete Account</UButton>
+    </section>
+
+</template>
